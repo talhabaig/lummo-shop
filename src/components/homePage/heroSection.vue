07901346972d087abffd208b74a8af0d1adf9ebc
talhabaig <baigtalha7@gmail.com>
2023-04-12 11:51:08 +0500

    update the hero section

---
+++ src/components/homePage/heroSection.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="bg-blue">
+    <div class="container">
+      <div
+        class="grid gap-8 sm:gap-32 md:gap-0 grid-cols-1 md:grid-cols-2 items-center row py-[25px] md:py-[50px]"
+      >
+        <div>
+          <div class="flex flex-col">
+            <h1
+              class="text-[2.3rem] lg:text-[3.1rem] leading-[2.875rem] lg:leading-[3.8rem] font-extrabold text-green mb-4"
+            >
+              Place of Business
+              <span class="text-white"> headscaffdfdf</span>
+              <span class="block"> Start Your Online Store, Take Control to Grow</span>
+            </h1>
+            <span class="text-white text-[0.9375rem]"
+              >With a complete & practical online store website, there are no more
+              barriers to optimizing your business growth opportunities.</span
+            >
+            <div class="flex md:flex-col lg:flex-row gap-3 mt-9">
+              <button
+                class="bg-white text-blue sm:py-3 sm:px-[1.85rem] py-2 px-3 text-[0.8rem] sm:text-[1rem] rounded-md border border-[#d1d5db] w-fit"
+              >
+                Create a demo Schedule
+              </button>
+              <button
+                class="bg-green text-blue text-[0.8rem] sm:text-[1rem] sm:py-3 sm:px-[1.85rem] py-2 px-3 rounded-md w-fit relative after:absolute after:top-[-15px] after:right-[-25px] after:px-[0.5rem] after:rounded-[16px] after:p-1 after:bg-white after:text-blue after:content-['Gratis'] after:text-[0.8rem] after:sm:text-[0.875rem] after:shadow-md"
+              >
+                From now on
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="pb-24 md:pb-0">
+            <img src="@/assets/img/hero-bg.png" alt="img" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,25 @@
 <template>
-    <div>
-        <banyakFitur/>
-        <langkahKecil/>
-        <cocokUntuk/>
-    </div>
+  <div>
+    <heroSection />
+    <banyakFitur />
+    <langkahKecil />
+    <cocokUntuk />
+  </div>
 </template>
 <script>
- import banyakFitur from "../components/homePage/banyakFitur.vue"
- import langkahKecil from "../components/homePage/langkahKecil.vue"
- import cocokUntuk from "../components/homePage/cocokUntuk.vue"
- export default {
-    components:{
-        banyakFitur,langkahKecil,cocokUntuk
-    },
-    data() {
-      return {  
-      };
-    },
-  };
- </script>
+import banyakFitur from "../components/homePage/banyakFitur.vue";
+import langkahKecil from "../components/homePage/langkahKecil.vue";
+import cocokUntuk from "../components/homePage/cocokUntuk.vue";
+import HeroSection from "../components/homePage/heroSection.vue";
+export default {
+  components: {
+    banyakFitur,
+    langkahKecil,
+    cocokUntuk,
+    HeroSection,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
